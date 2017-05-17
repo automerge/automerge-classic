9bda151e337b0da4d7fff0d054ea1b5b283c506c
Orion Henry <orion.henry@gmail.com>
2017-05-17 12:01:30 -0400

    first test

--- test/functional-test.js
+++ test/functional-test.js
@@ -1,18 +1,18 @@
 const assert = require('assert')
-const {Store} = require('../src/functional')
+const tesseract = require('../src/functional')
 
 describe('Functional Tesseract', () => {
   let s1, s2, s3
   beforeEach(() => {
-    s1 = Store('s1')
-    s2 = Store('s2')
-    s3 = Store('s3')
+    s1 = tesseract.init('s1')
+    s2 = tesseract.init('s2')
+    s3 = tesseract.init('s3')
   })
 
   describe('maps', () => {
     it('should allow property assignment', () => {
-      s1 = s1.assign({foo: 'foo'})
-      assert.equal(s1.root.foo, 'foo')
+      s1 = tesseract.set(s1,'foo','bar')
+      assert.equal(s1.foo, 'bar')
     })
   })
 })
