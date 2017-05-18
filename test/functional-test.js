c3fb551d955c7a8811c658af9b4c85b2acdd66c1
Orion Henry <orion.henry@gmail.com>
2017-05-18 10:44:45 -0400

    some more functional tests

--- test/functional-test.js
+++ test/functional-test.js
@@ -12,7 +12,26 @@ describe('Functional Tesseract', () => {
   describe('maps', () => {
     it('should allow property assignment', () => {
       s1 = tesseract.set(s1,'foo','bar')
+      s1 = tesseract.set(s1,'zip','zap')
       assert.equal(s1.foo, 'bar')
+      assert.deepEqual(s1, {'foo':'bar', 'zip':'zap' })
+    })
+    it('should allow be able to merge', () => {
+      s1 = tesseract.set(s1,'foo','bar')
+      s2 = tesseract.set(s2,'hello','world')
+      s3 = tesseract.merge(s1,s2)
+      assert.deepEqual(s3, {'foo':'bar', 'hello':'world' })
+    })
+    it('should allow be able to have nested objects', () => {
+      s1 = tesseract.set(s1,'foo',{"hello":"world"})
+      assert.deepEqual(s1, {'foo':{'hello':'world' }})
+      s2 = tesseract.set(s2,'aaa',{"bbb":"ccc"})
+      assert.deepEqual(s2, {'aaa':{'bbb':'ccc'}})
+      s3 = tesseract.merge(s3,s2)
+      s3 = tesseract.merge(s3,s1)
+      assert.deepEqual(s3, {'foo':{'hello':'world'}, 'aaa':{'bbb':'ccc'}})
+      s3 = tesseract.set(s3.foo,'key','val')
+      assert.deepEqual(s3, {'foo':{'key':'val','hello':'world'}, 'aaa':{'bbb':'ccc'}})
     })
   })
 })
