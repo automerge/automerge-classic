aaee2adaf19af8b85ad19ebc9180b81fee4e2f74
Martin Kleppmann <martin@kleppmann.com>
2017-09-10 16:42:18 +0100

    Clarify documentation of vector clock comparison

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,8 @@ const { Map, fromJS } = require('immutable')
 const OpSet = require('./op_set')
 
 // Returns true if all components of clock1 are less than or equal to those of clock2.
-// Returns false if the clocks are incomparable or if clock1 is greater than clock2.
+// Returns false if there is at least one component in which clock1 is greater than clock2
+// (that is, either clock1 is overall greater than clock2, or the clocks are incomparable).
 function lessOrEqual(clock1, clock2) {
   return clock1.keySeq().concat(clock2.keySeq()).reduce(
     (result, key) => (result && clock1.get(key, 0) <= clock2.get(key, 0)),
