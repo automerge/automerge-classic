6de5a6d1719104fc3667b51906960b7cc96bbdf1
Ethan Brown <ethan@vms-inc.com>
2018-04-18 15:48:00 -0700

    Improved (published) package compatibility.
    * Configured webpack to target umd, which covers Node, global browser
    use, and use with ESM
    * Configured webpack to transcompile with Babel (using env preset),
    making suitable for use with create-react-app, and to generally improve
    compatibility
    * Configured webpack to generate source map
    * Modified scripts to be more standard ("build" instead of "webpack",
    "prepublishOnly" to prevent forgetful publishing of old versions; note
    that "prepublish" is deprecated as of npm 5 in favor of
    "prepublishOnly")

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/automerge.js',
+  entry: ['babel-polyfill', './src/automerge.js'],
   output: {
     filename: 'automerge.js',
     library: 'Automerge',
+    libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   }
 }
