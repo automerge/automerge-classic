bfd3878a278138595d00caf30a1115dd35dfd34c
Samy Alzahrani <samy@jehazy.com>
2018-06-18 22:37:29 +0300

    Upgrade to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ var path = require('path');
 
 module.exports = {
   entry: './src/automerge.js',
+  mode: 'development',
   output: {
     filename: 'automerge.js',
     library: 'Automerge',
     libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    // https://github.com/webpack/webpack/issues/6525
+    globalObject: 'this'
   },
   devtool: 'source-map',
   module: {
