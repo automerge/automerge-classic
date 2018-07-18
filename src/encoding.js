bfa56a244332f77377c8f6fd6814a57907341589
Martin Kleppmann <martin@kleppmann.com>
2018-07-18 16:19:27 +0100

    Basic binary encoders/decoders

---
+++ src/encoding.js
@@ -0,0 +1,215 @@
+const leb = require('leb') // https://en.wikipedia.org/wiki/LEB128
+
+// The TextEncoder API is provided natively by Chrome and Firefox, but not by Node.
+// https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+//
+// FIXME: The text-encoding package includes a file called encoding-indexes.js containing
+// translation tables for various charsets. It's about 500 kB in size and we don't need it, since we
+// only use UTF8 and no other encoding. It bloats the bundled webpack output so it would be good to
+// omit it if possible. But I don't know how to configure webpack accordingly.
+// Some discussion here: https://github.com/inexorabletash/text-encoding/issues/44
+const { TextEncoder, TextDecoder } = require('text-encoding')
+const utf8encoder = new TextEncoder()
+
+
+// A byte array to which new data (also given as a byte array) can be appended.
+// Dynamically grows the byte array as required.
+class Encoder {
+  constructor (valueEncoder) {
+    this.valueEncoder = valueEncoder
+    this.buf = new Uint8Array(16)
+    this.offset = 0
+  }
+
+  write (data) {
+    if (this.offset + data.byteLength >= this.buf.byteLength) {
+      const newBuf = new Uint8Array(this.buf.byteLength * 4)
+      newBuf.set(this.buf, 0)
+      this.buf = newBuf
+    }
+
+    this.buf.set(data, this.offset)
+    this.offset += data.byteLength
+  }
+
+  writeValue (value) {
+    this.write(this.valueEncoder(value))
+  }
+
+  writeUInt32 (value) {
+    this.write(leb.encodeUInt32(value))
+  }
+
+  writeInt32 (value) {
+    this.write(leb.encodeInt32(value))
+  }
+
+  flush () {
+  }
+}
+
+
+// Decodes data in a byte array into values.
+class Decoder {
+  constructor (valueDecoder, buf, length) {
+    this.valueDecoder = valueDecoder
+    this.buf = buf
+    this.offset = 0
+    this.length = (typeof length === 'number') ? length : buf.byteLength
+  }
+
+  hasMore () {
+    return this.offset < this.length
+  }
+
+  readValue () {
+    const { value, nextIndex } = this.valueDecoder(this.buf, this.offset)
+    this.offset = nextIndex
+    return value
+  }
+
+  readUInt32 () {
+    const { value, nextIndex } = leb.decodeUInt32(this.buf, this.offset)
+    this.offset = nextIndex
+    return value
+  }
+
+  readInt32 () {
+    const { value, nextIndex } = leb.decodeInt32(this.buf, this.offset)
+    this.offset = nextIndex
+    return value
+  }
+}
+
+
+// A run-length encoder that compactly encodes repeated values; the function to encode a value is
+// passed to the constructor. The encoding starts with a LEB128-encoded signed integer. If it is
+// positive number n, the following value is repeated n times. If it is a negative number n, the
+// following -n values each appear once.
+class RLEEncoder extends Encoder {
+  constructor (valueEncoder) {
+    super(valueEncoder)
+    this.values = []
+    this.count = 0
+  }
+
+  writeValue (value) {
+    if (this.values.length === 0) {
+      this.values.push(value)
+    }
+
+    if (this.values[this.values.length - 1] === value) {
+      // Value is repeated
+      this.count += 1
+      if (this.values.length > 1) {
+        this.writeIndividualValues(this.values.splice(0, this.values.length - 1))
+      }
+
+    } else if (this.count === 1) {
+      // Value is different from before, and previous value occurred only once
+      this.values.push(value)
+
+    } else {
+      // Value is different from before, and previous value was repeated
+      this.writeRepeatedValue()
+      this.values[0] = value
+      this.count = 1
+    }
+  }
+
+  writeIndividualValues (values) {
+    this.writeInt32(-values.length)
+    for (const value of values) {
+      super.writeValue(value)
+    }
+  }
+
+  writeRepeatedValue () {
+    if (this.values.length !== 1) {
+      throw new Error('This should never happen')
+    }
+    this.writeInt32(this.count)
+    super.writeValue(this.values[0])
+  }
+
+  flush () {
+    if (this.count > 1) {
+      this.writeRepeatedValue()
+    } else if (this.values.length > 1) {
+      this.writeIndividualValues(this.values)
+    }
+    this.values = []
+    this.count = 0
+  }
+}
+
+
+// Decodes data in a byte array encoded by RLEEncoder.
+class RLEDecoder extends Decoder {
+  constructor (valueDecoder, buf, length) {
+    super(valueDecoder, buf, length)
+    this.repeats = 0
+    this.individuals = 0
+  }
+
+  hasMore () {
+    return (this.repeats > 0) || (this.individuals > 0) || super.hasMore()
+  }
+
+  readValue () {
+    if (this.repeats === 0 && this.individuals === 0) {
+      const count = this.readInt32()
+      if (count === 0) {
+        throw new RangeError('Zero RLE count is not allowed')
+      } else if (count > 0) {
+        this.repeats = count
+        this.lastValue = super.readValue()
+      } else {
+        this.individuals = -count
+      }
+    }
+
+    if (this.repeats > 0) {
+      this.repeats -= 1
+      return this.lastValue
+    }
+
+    if (this.individuals > 0) {
+      this.individuals -= 1
+      return super.readValue()
+    }
+  }
+}
+
+
+// An encoder for numbers that tend to appear in incrementing sequences. For each value, it takes
+// the difference to the previous value, and run-length encodes that sequence of differences.
+class DeltaEncoder extends RLEEncoder {
+  constructor () {
+    super(leb.encodeInt32)
+    this.lastInput = 0
+  }
+
+  writeValue (input) {
+    super.writeValue(input - this.lastInput)
+    this.lastInput = input
+  }
+}
+
+// A decoder for data encoded by DeltaEncoder
+class DeltaDecoder extends RLEDecoder {
+  constructor (buf, length) {
+    super(leb.decodeInt32, buf, length)
+    this.lastOutput = 0
+  }
+
+  readValue () {
+    this.lastOutput += super.readValue()
+    return this.lastOutput
+  }
+}
+
+
+module.exports = {
+  RLEEncoder, RLEDecoder, DeltaEncoder, DeltaDecoder
+}
---
+++ test/encoding_test.js
@@ -0,0 +1,149 @@
+const assert = require('assert')
+const { RLEEncoder, RLEDecoder, DeltaEncoder, DeltaDecoder } = require('../src/encoding')
+const leb = require('leb')
+
+describe('Binary encoding', () => {
+  describe('RLEEncoder', () => {
+    it('encodes no values as an empty byte string', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 0)
+    })
+
+    it('encodes a repeated value with a counter', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 50; i++) encoder.writeValue(42)
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 2)
+      assert.deepEqual(encoder.buf.subarray(0, 2), [50, 42])
+    })
+
+    it('encodes non-repeated values with a prefix', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 10; i++) encoder.writeValue(i)
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 11)
+      assert.deepEqual(encoder.buf.subarray(0, 11), [0x80 - 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+
+    it('allows mixing repeated and non-repeated values', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      encoder.writeValue(1)
+      encoder.writeValue(2)
+      encoder.writeValue(3); encoder.writeValue(3); encoder.writeValue(3)
+      encoder.writeValue(4); encoder.writeValue(4)
+      encoder.writeValue(5)
+      encoder.writeValue(6)
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 10)
+      assert.deepEqual(encoder.buf.subarray(0, 10), [0x80 - 2, 1, 2, 3, 3, 2, 4, 0x80 - 2, 5, 6])
+    })
+  })
+
+  describe('RLEDecoder', () => {
+    it('decodes an empty byte string as no values', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      encoder.flush()
+      const decoder = new RLEDecoder(leb.decodeInt32, encoder.buf, encoder.offset)
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('decodes a repeated value', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 4; i++) encoder.writeValue(42)
+      encoder.flush()
+      const decoder = new RLEDecoder(leb.decodeInt32, encoder.buf, encoder.offset)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 42)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 42)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 42)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 42)
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('decodes non-repeated values', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 10; i++) encoder.writeValue(i)
+      encoder.flush()
+      const decoder = new RLEDecoder(leb.decodeInt32, encoder.buf, encoder.offset)
+      for (let i = 0; i < 10; i++) {
+        assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), i)
+      }
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('decodes a mixture of repeated and non-repeated values', () => {
+      const decoder = new RLEDecoder(leb.decodeInt32, new Uint8Array([126, 1, 2, 3, 3, 2, 4, 126, 5, 6]))
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 1)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 2)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 3)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 3)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 3)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 4)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 4)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 5)
+      assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 6)
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('performs a round-trip with large values', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 1000; i++) encoder.writeValue(123456789)
+      encoder.flush()
+      const decoder = new RLEDecoder(leb.decodeInt32, encoder.buf, encoder.offset)
+      for (let i = 0; i < 1000; i++) {
+        assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), 123456789)
+      }
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('performs a round-trip with lots of values', () => {
+      const encoder = new RLEEncoder(leb.encodeInt32)
+      for (let i = 0; i < 1000; i++) encoder.writeValue(i)
+      encoder.flush()
+      const decoder = new RLEDecoder(leb.decodeInt32, encoder.buf, encoder.offset)
+      for (let i = 0; i < 1000; i++) {
+        assert.strictEqual(decoder.hasMore(), true); assert.strictEqual(decoder.readValue(), i)
+      }
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+  })
+
+  describe('DeltaEncoder / DeltaDecoder', () => {
+    it('encodes/decodes a sequence of no values', () => {
+      const encoder = new DeltaEncoder()
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 0)
+      const decoder = new DeltaDecoder(encoder.buf, encoder.offset)
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+
+    it('encodes an incrementing sequence', () => {
+      const encoder = new DeltaEncoder()
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 20; j++) encoder.writeValue(j)
+      }
+      encoder.flush()
+      assert.strictEqual(encoder.offset, 12)
+      assert.deepEqual(encoder.buf.subarray(0, 12), [
+                       127, 0,         19, 1,
+                       127, 0x80 - 19, 19, 1,
+                       127, 0x80 - 19, 19, 1])
+    })
+
+    it('performs a round-trip on incrementing sequences', () => {
+      const encoder = new DeltaEncoder()
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 1000; j++) encoder.writeValue(j)
+      }
+      encoder.flush()
+      const decoder = new DeltaDecoder(encoder.buf, encoder.offset)
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 1000; j++) {
+          assert.strictEqual(decoder.hasMore(), true)
+          assert.strictEqual(decoder.readValue(), j)
+        }
+      }
+      assert.strictEqual(decoder.hasMore(), false)
+    })
+  })
+})
