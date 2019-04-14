2cdf14cbca49fc1506a3115eb1793a6b2cbe2caf
Herb Caudill <herb@devresults.com>
2019-04-14 14:04:10 +0200

    restore uuid factory, add typings

--- src/uuid.js
+++ src/uuid.js
@@ -1,1 +1,12 @@
-module.exports = require('uuid/v4')
+const uuid = require('uuid/v4')
+
+let factory = uuid
+
+function makeUuid() {
+  return factory()
+}
+
+makeUuid.setFactory = newFactory => factory = newFactory
+makeUuid.reset = () => factory = uuid
+
+module.exports = makeUuid
--- test/uuid_test.ts
+++ test/uuid_test.ts
@@ -1,10 +1,30 @@
 import * as assert from 'assert'
-import uuid from 'uuid'
+import { uuid } from 'automerge'
 
 describe('uuid', () => {
+  afterEach(() => {
+    uuid.reset()
+  })
+
   describe('default implementation', () => {
     it('generates unique values', () => {
       assert.notEqual(uuid(), uuid())
+    })
+  })
+
+  describe('custom implementation', () => {
+    let counter: number
+
+    function customUuid() {
+      return `custom-uuid-${counter++}`
+    }
+
+    before(() => uuid.setFactory(customUuid))
+    beforeEach(() => counter = 0)
+
+    it('invokes the custom factory', () => {
+      assert.equal(uuid(), 'custom-uuid-0');
+      assert.equal(uuid(), 'custom-uuid-1');
     })
   })
 })
