964d5504f074e3b0bc6f686bdad439cbc04a19cd
Herb Caudill <herb@devresults.com>
2019-06-29 12:08:06 +0200

    group types functionally

--- @types/automerge/index.d.ts
+++ @types/automerge/index.d.ts
@@ -1,16 +1,18 @@
 declare module 'automerge' {
   type Doc<T> = DeepFreezeObject<T>
-  type DocShape<D> = D extends Doc<infer T> ? T : never;
+  type DocShape<D> = D extends Doc<infer T> ? T : never
 
-  function applyChanges<D, T = DocShape<D>>(doc: D, changes: Change<T>[]): D
-  function canRedo<T>(doc: Doc<T>): boolean
-  function canUndo<T>(doc: Doc<T>): boolean
+  function init<T>(actorId?: string): Doc<T>
+  function from<T>(initialState: T | Doc<T>): Doc<T>
+  function merge<T>(localdoc: Doc<T>, remotedoc: Doc<T>): Doc<T>
+
   function change<D, T = DocShape<D>>(doc: D, message: string, callback: ChangeFn<T>): D
   function change<D, T = DocShape<D>>(doc: D, callback: ChangeFn<T>): D
-  function diff<D extends Doc<any>>(olddoc: D, newdoc: D): Diff[]
   function emptyChange<D extends Doc<any>>(doc: D, message?: string): D
+  function applyChanges<D, T = DocShape<D>>(doc: D, changes: Change<T>[]): D
+  function diff<D extends Doc<any>>(olddoc: D, newdoc: D): Diff[]
   function equals<T>(val1: T, val2: T): boolean
-  function from<T>(initialState: T | Doc<T>): Doc<T>
+
   function getActorId<T>(doc: Doc<T>): string
   function getChanges<D, T = DocShape<D>>(olddoc: D, newdoc: D): Change<T>[]
   function getConflicts<T>(doc: Doc<T>, key: Key): any
@@ -18,11 +20,14 @@ declare module 'automerge' {
   function getMissingDeps<T>(doc: Doc<T>): Clock
   function getObjectById<T>(doc: Doc<T>, objectId: UUID): any
   function getObjectId(object: any): UUID
-  function init<T>(actorId?: string): Doc<T>
+
   function load<T>(doc: string, actorId?: string): Doc<T>
-  function merge<T>(localdoc: Doc<T>, remotedoc: Doc<T>): Doc<T>
-  function redo<T>(doc: Doc<T>, message?: string): Doc<T>
   function save<T>(doc: Doc<T>): string
+
+  function canRedo<T>(doc: Doc<T>): boolean
+  function canUndo<T>(doc: Doc<T>): boolean
+
+  function redo<T>(doc: Doc<T>, message?: string): Doc<T>
   function undo<T>(doc: Doc<T>, message?: string): Doc<T>
 
   class Connection<T> {
@@ -37,12 +42,7 @@ declare module 'automerge' {
 
   class Table<T, KeyOrder extends Array<keyof T>> {
     constructor(columns: KeyArray<T, KeyOrder>)
-    [Symbol.iterator](): {
-      next: () => {
-        done: boolean
-        value: T
-      }
-    }
+    [Symbol.iterator](): Iterator<T>
     add(item: T | TupleFromInterface<T, KeyOrder>): UUID
     byId(id: UUID): T
     columns: string[]
@@ -58,21 +58,10 @@ declare module 'automerge' {
     sort(arg?: Function | string | string[]): void
   }
 
-  type ReadonlyTable<T, KeyOrder extends Array<keyof T>> = Omit<Table<T, KeyOrder>, 'add' | 'remove' | 'set' | 'sort'> & {
-    [Symbol.iterator](): {
-      next: () => {
-        done: boolean
-        value: T
-      }
-    },
-  }
-
   class List<T> extends Array<T> {
     insertAt?(index: number, ...args: T[]): List<T>
     deleteAt?(index: number, numDelete?: number): List<T>
   }
-
-  type ReadonlyList<T> = ReadonlyArray<T>;
 
   class Text extends List<string> {
     constructor(objectId?: UUID, elems?: string[], maxElem?: number)
@@ -80,7 +69,29 @@ declare module 'automerge' {
     getElemId(index: number): string
   }
 
-  type ReadonlyText = ReadonlyList<string> &  {
+  // Note that until https://github.com/Microsoft/TypeScript/issues/2361 is addressed, we
+  // can't treat a Counter like a literal number without force-casting it as a number.
+  // This won't compile:
+  //   `assert.strictEqual(c + 10, 13) // Operator '+' cannot be applied to types 'Counter' and '10'.ts(2365)`
+  // But this will:
+  //   `assert.strictEqual(c as unknown as number + 10, 13)`
+  class Counter extends Number {
+    constructor(value?: number)
+    increment(delta?: number): void
+    decrement(delta?: number): void
+    toString(): string
+    valueOf(): number
+    value: number
+  }
+
+  type ReadonlyTable<T, KeyOrder extends Array<keyof T>> = Omit<
+    Table<T, KeyOrder>,
+    'add' | 'remove' | 'set' | 'sort'
+  > & {
+    [Symbol.iterator](): Iterator<T>
+  }
+  type ReadonlyList<T> = ReadonlyArray<T>
+  type ReadonlyText = ReadonlyList<string> & {
     get(index: number): string
     getElemId(index: number): string
   }
@@ -103,26 +114,15 @@ declare module 'automerge' {
     unregisterHandler(handler: Handler<T>): void
   }
 
-  // Note that until https://github.com/Microsoft/TypeScript/issues/2361 is addressed, we
-  // can't treat a Counter like a literal number without force-casting it as a number.
-  // This won't compile:
-  //   `assert.strictEqual(c + 10, 13) // Operator '+' cannot be applied to types 'Counter' and '10'.ts(2365)`
-  // But this will:
-  //   `assert.strictEqual(c as unknown as number + 10, 13)`
-  class Counter extends Number {
-    constructor(value?: number)
-    increment(delta?: number): void
-    decrement(delta?: number): void
-    toString(): string
-    valueOf(): number
-    value: number
-  }
-
   namespace Frontend {
     function applyPatch<T>(doc: Doc<T>, patch: Patch): Doc<T>
     function canRedo<T>(doc: Doc<T>): boolean
     function canUndo<T>(doc: Doc<T>): boolean
-    function change<D, T = DocShape<D>>(doc: D, message: string | undefined, callback: ChangeFn<T>): [T, Change<T>]
+    function change<D, T = DocShape<D>>(
+      doc: D,
+      message: string | undefined,
+      callback: ChangeFn<T>
+    ): [T, Change<T>]
     function change<D, T = DocShape<D>>(doc: D, callback: ChangeFn<T>): [D, Change<T>]
     function emptyChange<T>(doc: Doc<T>, message?: string): [Doc<T>, Change<T>]
     function from<T>(initialState: T | Doc<T>): [Doc<T>, Change<T>]
@@ -230,7 +230,7 @@ declare module 'automerge' {
   }
 
   type RequestType =
-    | 'change'
+    | 'change' //..
     | 'redo'
     | 'undo'
 
@@ -246,50 +246,50 @@ declare module 'automerge' {
     | 'makeMap'
 
   type DiffAction =
-    | 'create'
+    | 'create' //..
     | 'insert'
     | 'set'
     | 'remove'
 
   type CollectionType =
-    | 'list'
+    | 'list' //..
     | 'map'
     | 'table'
     | 'text'
 
-  type DataType = 'counter' | 'timestamp'
-
+  type DataType =
+    | 'counter' //..
+    | 'timestamp' 
 
   // TYPE UTILITY FUNCTIONS
 
   // Type utility function: DeepFreeze
   // Generates a readonly version of a given object, array, or map type applied recursively to the nested members of the root type.
   // It's like TypeScript's `readonly`, but goes all the way down a tree.
-  type DeepFreeze<T> =
-    T extends Function ? T :
-    T extends Text ? ReadonlyText :
-    T extends List<infer E> ? DeepFreezeList<E> :
-    T extends Table<infer R, infer KeyOrder> ? DeepFreezeTable<R, KeyOrder> :
-    T extends ReadonlyArray<infer R> ? DeepFreezeArray<R> :
-    T extends Map<infer K, infer V> ? DeepFreezeMap<K, V> :
-    DeepFreezeObject<T> 
 
-  interface DeepFreezeList<E> extends ReadonlyList<DeepFreeze<E>> {}
-  interface DeepFreezeTable<R, KeyOrder> extends ReadonlyTable<DeepFreeze<R>, Array<keyof DeepFreeze<R>>> {}
+  // prettier-ignore
+  type DeepFreeze<T> = 
+    T extends Function ? T
+    : T extends Text ? ReadonlyText
+    : T extends List<infer E> ? DeepFreezeList<E>
+    : T extends Table<infer R, infer KeyOrder> ? DeepFreezeTable<R, KeyOrder>
+    : T extends ReadonlyArray<infer R> ? DeepFreezeArray<R>
+    : T extends Map<infer K, infer V> ? DeepFreezeMap<K, V>
+    : DeepFreezeObject<T>
+
+  interface DeepFreezeList<T> extends ReadonlyList<DeepFreeze<T>> {}
+  interface DeepFreezeTable<T, KeyOrder>
+    extends ReadonlyTable<DeepFreeze<T>, Array<keyof DeepFreeze<T>>> {}
   interface DeepFreezeArray<T> extends ReadonlyArray<DeepFreeze<T>> {}
   interface DeepFreezeMap<K, V> extends ReadonlyMap<DeepFreeze<K>, DeepFreeze<V>> {}
   type DeepFreezeObject<T> = { readonly [P in keyof T]: DeepFreeze<T[P]> }
-
 }
-
-type Lookup<T, K> = K extends keyof T ? T[K] : never
 
 // Type utility function: KeyArray
 // Enforces that the array provided for key order only contains keys of T
-type KeyArray<T, KeyOrder extends Array<keyof T>> =
-  keyof T extends KeyOrder[number]
-    ? KeyOrder
-    : Exclude<keyof T, KeyOrder[number]>[]
+type KeyArray<T, KeyOrder extends Array<keyof T>> = keyof T extends KeyOrder[number]
+  ? KeyOrder
+  : Exclude<keyof T, KeyOrder[number]>[]
 
 // Type utility function: TupleFromInterface
 // Generates a tuple containing the types of each property of T, in the order provided by KeyOrder. For example:
@@ -309,4 +309,8 @@ type KeyArray<T, KeyOrder extends Array<keyof T>> =
 // add({authors, title, date}) // valid
 // add([authors, title, date]) // also valid
 // ```
-type TupleFromInterface<T, KeyOrder extends Array<keyof T>> = { [I in keyof KeyOrder]: Lookup<T, KeyOrder[I]> }
+type TupleFromInterface<T, KeyOrder extends Array<keyof T>> = {
+  [I in keyof KeyOrder]: Lookup<T, KeyOrder[I]>
+}
+
+type Lookup<T, K> = K extends keyof T ? T[K] : never
