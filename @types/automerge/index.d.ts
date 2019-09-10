8cd8784bdbb9fd1f50eda60bef3d8dcdf01c8b4b
Peter van Hardenberg <pvh@pvh.ca>
2019-09-10 16:54:41 -0700

    okay, maybe not any

--- @types/automerge/index.d.ts
+++ @types/automerge/index.d.ts
@@ -76,7 +76,7 @@ declare module 'automerge' {
     constructor(objectId?: UUID, elems?: string[], maxElem?: number)
     get(index: number): string
     getElemId(index: number): string
-    toSpans(): (string | any)[]
+    toSpans<T>(): (string | T)[]
   }
 
   // Note that until https://github.com/Microsoft/TypeScript/issues/2361 is addressed, we
