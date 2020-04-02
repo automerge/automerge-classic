78c9372a00183680efe09300f305cbe3e389b411
Martin Kleppmann <martin@kleppmann.com>
2020-04-02 21:41:56 +0100

    Provide encodeChange/decodeChanges to wasm wrapper
    Depends on automerge/automerge-rs@2d7190394d5a087cecb430bce8ec42dddd12aca4

--- test/wasm.js
+++ test/wasm.js
@@ -9,12 +9,14 @@
 // currently only works in Node, not in a browser.
 
 const Automerge = process.env.TEST_DIST === '1' ? require('../dist/automerge') : require('../src/automerge')
+const CodecFunctions = require('../backend/columnar')
 const path = require('path')
 
 if (process.env.WASM_BACKEND_PATH) {
-  const MODULE_PATH = path.resolve(process.env.WASM_BACKEND_PATH)
+  const wasmBackend = require(path.resolve(process.env.WASM_BACKEND_PATH))
+  wasmBackend.initCodecFunctions(CodecFunctions)
   Automerge._js_backend = Automerge.Backend
-  Automerge.setDefaultBackend(require(MODULE_PATH))
+  Automerge.setDefaultBackend(wasmBackend)
 } else {
   throw new RangeError('Please set environment variable WASM_BACKEND_PATH to the path of the WebAssembly backend')
 }
