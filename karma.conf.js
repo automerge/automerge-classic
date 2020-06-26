7fc7c0025095b7b34c222b85ce2ea0b891389ea8
NikitaIT <NIKITA2008-101@yandex.ru>
2020-06-26 20:15:24 +0300

    Add karma typescript support.
    Add 'src' to 'files' because .ts files should be transpiled.

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,16 @@
+
 module.exports = function(config) {
   config.set({
     frameworks: ['browserify', 'mocha', 'karma-typescript'],
-    files: ['test/*test*.js', 'test/*test*.ts'],
+    files: ['test/*test*.js', 'test/*test*.ts', { pattern: "frontend/**/*.+(js|ts)" }, { pattern: "src/**/*.+(js|ts)" }, { pattern: "backend/**/*.+(js|ts)" }],
     preprocessors: {
-      'test/*.js': ['browserify'],
-      'test/*.ts': ['karma-typescript']
+      "src/**/*.+(js|ts)": ["karma-typescript"],
+      "frontend/**/*.+(js|ts)": ["karma-typescript"],
+      "backend/**/*.+(js|ts)": ["karma-typescript"],
+      'test/*.+(ts|js)': ['karma-typescript']
     },
     browserify: {debug: true},
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: ['Chrome', 'Firefox'],
     singleRun: true,
     karmaTypescriptConfig: {
       tsconfig: './tsconfig.json',
