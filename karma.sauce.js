1bb53965489ebf385105ac8a1f29d364c09eb471
Gen Hames <hamesg@tutanota.de>
2021-02-24 15:50:55 -0800

    Update packages and move from Browserify to Webpack for Karma

--- karma.sauce.js
+++ karma.sauce.js
@@ -1,3 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require("./webpack.config.js")
+
+// Karma-Webpack needs these gone
+delete webpackConfig.entry;
+delete webpackConfig.output.filename;
+
+// Minify is unnecessary and adds about 15s of work
+delete webpackConfig.optimization
+
+// Don't mix dist/
+webpackConfig.output.path = path.join(webpackConfig.output.path, 'test')
+
+// You're importing *a lot* of Node-specific code so the bundle is huge...
+webpackConfig.plugins = [
+  new webpack.DefinePlugin({
+    'process.env.TEST_DIST': JSON.stringify(process.env.TEST_DIST) || '1',
+    'process.env.NODE_DEBUG': false,
+  }),
+  ...(webpackConfig.plugins || []),
+];
+
 module.exports = function(config) {
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
     console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
@@ -34,19 +57,22 @@ module.exports = function(config) {
   }
 
   config.set({
-    frameworks: ['browserify', 'mocha', 'karma-typescript'],
-    files: ['test/*test*.js', 'test/*test*.ts'],
+    frameworks: ['webpack', 'mocha', 'karma-typescript'],
+    files: [
+      { pattern: 'test/*test*.js', watched: false },
+      { pattern: 'test/*test*.ts' },
+    ],
     preprocessors: {
-      'test/*.js': ['browserify'],
-      'test/*.ts': ['karma-typescript']
+      'test/*test*.js': ['webpack'],
+      'test/*test*.ts': ['karma-typescript'],
     },
+    webpack: webpackConfig,
     karmaTypescriptConfig: {
       tsconfig: './tsconfig.json',
       compilerOptions: {
         sourceMap: true,
       }
     },
-    browserify: {debug: true},
     port: 9876,
     captureTimeout: 120000,
     sauceLabs: {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path');
-const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/automerge.js',
@@ -10,16 +11,36 @@ module.exports = {
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
     // https://github.com/webpack/webpack/issues/6525
-    globalObject: 'this'
+    globalObject: 'this',
   },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+  ],
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    ],
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()]
-  }
-}
+    minimizer: [new TerserPlugin()],
+  },
+};
