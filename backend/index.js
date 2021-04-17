22e83d09c7b9e80ae6d96a7911969e558570a2b6
Peter van Hardenberg <pvh@pvh.ca>
2021-04-17 09:01:41 -0700

    combine sync/protocol files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 
 const { init, clone, free, applyChanges, applyLocalChange, save, load, loadChanges, getPatch, getHeads, getAllChanges, getChanges, getChangeByHash, getMissingDeps } = require("./backend")
-const { receiveSyncMessage, generateSyncMessage } = require('./protocol')
-const { encodeSyncMessage, decodeSyncMessage, encodeSyncState, decodeSyncState, makeBloomFilter, getChangesToSend } = require('./sync')
+const { receiveSyncMessage, generateSyncMessage, encodeSyncMessage, decodeSyncMessage, encodeSyncState, decodeSyncState, makeBloomFilter, getChangesToSend } = require('./sync')
 
 module.exports = {
   init, clone, free, applyChanges, applyLocalChange, save, load, loadChanges, getPatch,
