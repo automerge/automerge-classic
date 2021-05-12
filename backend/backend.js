744939f6b96f80c6ceb6debbf63a2bed244ba0eb
Martin Kleppmann <martin@kleppmann.com>
2021-05-12 17:02:50 +0100

    Avoid all uses of Immutable.js .toJSON() method
    We actually only ever want shallow conversion, so using .toArray() or
    .toObject() is safer.

--- backend/backend.js
+++ backend/backend.js
@@ -52,8 +52,8 @@ function free(backend) {
  * object modifications `diffs`.
  */
 function makePatch(state, diffs, request, isIncremental) {
-  const clock = state.getIn(['opSet', 'states']).map(seqs => seqs.size).toJSON()
-  const deps  = state.getIn(['opSet', 'deps']).toJSON().sort()
+  const clock = state.getIn(['opSet', 'states']).map(seqs => seqs.size).toObject()
+  const deps  = state.getIn(['opSet', 'deps']).toArray().sort()
   const maxOp = state.getIn(['opSet', 'maxOp'], 0)
   const pendingChanges = OpSet.getMissingDeps(state.get('opSet')).length
   const patch = {clock, deps, diffs, maxOp, pendingChanges}
