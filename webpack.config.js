96001e7b797f37750f8dd8e693f3b8070ac23db4
Scott <scott@scotttrinh.com>
2021-05-27 09:00:27 -0400

    Do not chunk the output in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // https://github.com/webpack/webpack/issues/6525
     globalObject: 'this',
+    // https://github.com/webpack/webpack/issues/11660
+    chunkLoading: false,
   },
   devtool: 'source-map',
   module: {rules: []},
